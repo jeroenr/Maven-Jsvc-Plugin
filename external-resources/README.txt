2bfd69e88324c74fdefde5a526d82715ce8b4a7b
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2011-04-21 20:23:08 +0200

    Set up mojo skeleton

---
+++ external-resources/README.txt
@@ -0,0 +1,1 @@
+Put external resources for your application here.
---
+++ src/test/java/daemon/plugin/maven/JsvcMojoTest.java
@@ -0,0 +1,1 @@
+package daemon.plugin.maven;import org.junit.Before;import org.junit.Test;public class JsvcMojoTest {    @Before    public void setUp() throws Exception {    }    @Test    public void shouldExecute() throws Exception {    }}
